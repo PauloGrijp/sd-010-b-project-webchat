489c12228178cd4a0b518460440358e0543cc88b
Daniel <dsouzaborini@gmail.com>
2021-10-26 16:54:23 -0300

    add basic config to server, and middlewares!

--- server.js
+++ server.js
@@ -1,1 +1,45 @@
-// Faça seu código aqui :D
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const controllers = require('./controllers');
+const middlewares = require('./middlewares');
+
+const { PORT = 3000, SOCKET_PORT = 4555 } = process.env;
+
+const socketIoServer = require('http').createServer();
+
+const MSGS = [];
+
+const io = require('socket.io')(socketIoServer, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST'],
+  },
+});
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(
+  cors({
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Authorization'],
+  }),
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(middlewares.error);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
+
+socketIoServer.listen(SOCKET_PORT);
