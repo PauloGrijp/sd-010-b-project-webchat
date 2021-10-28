45f9d8b630a110092f418e5482b073670322bb06
dennisMarcelo <dennismarcelo15@gmail.com>
2021-10-28 01:16:06 -0300

    console.log removed

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -3,8 +3,6 @@ const chatModel = require('../models/chatModel');
 const inialPage = async (req, res) => {
   const messages = await chatModel.findAll() || [];
 
-  console.log(messages);
-
   res.status(200).render('chat', { messages });
 };
 
