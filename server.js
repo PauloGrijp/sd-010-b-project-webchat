2d23d0ec61ab8e773a582429b0a2ead1f2fe82fc
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-10-29 20:44:09 -0300

    refactor server

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ require('dotenv').config();
 const { PORT } = process.env || 3000;
 
 const app = express();
-app.use(express.static(`${__dirname}/src`));
-app.get('/', (_req, res) => {
-  res.sendFile(`${__dirname}/src/views/chat.html`);
-});
+app.use(express.json());
+// app.use(express.static(`${__dirname}/src`));
+// app.get('/', (_req, res) => {
+//   res.sendFile(`${__dirname}/src/views/chat.html`);
+// });
 
 const http = require('http').createServer(app);
 
@@ -20,9 +21,16 @@ const io = require('socket.io')(http, {
 require('./src/sockets/messages')(io);
 require('./src/sockets/users')(io);
 
-app.get('/', (_req, res) => {
-  res.sendFile(`${__dirname}/src/views/chat.html`);
-});
+// app.get('/', (_req, res) => {
+//   res.sendFile(`${__dirname}/src/views/chat.html`);
+// });
+
+const messagesController = require('./controllers/messagesController');
+
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
+app.get('/', messagesController.GetHistoricMessages);
 
 http.listen(3000, () => {
   console.log('Server on: port 3000');
