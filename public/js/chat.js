22b1cae67149b64548cbe0de011f564a83ba5316
Diegho <dieghonm@gmail.com>
2021-10-30 14:42:25 -0300

    Requisito 2, cria front

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,12 +1,21 @@
 const socket = window.io();
 
-const form = document.querySelector('form');
+const messageForm = document.querySelector('.messageForm');
 const inputMessage = document.querySelector('#messageInput');
+const nickForm = document.querySelector('.nickForm');
+const inputNick = document.querySelector('#nickInput');
 
-form.addEventListener('submit', (e) => {
+messageForm.addEventListener('submit', (e) => {
   e.preventDefault();
   socket.emit('message', { chatMessage: inputMessage.value });
   inputMessage.value = '';
+  return false;
+});
+
+nickForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  socket.emit('nick', inputNick.value);
+  inputNick.value = '';
   return false;
 });
 
@@ -14,23 +23,33 @@ const createMessage = (message) => {
   const messagesUl = document.querySelector('#messages');
   const li = document.createElement('li');
   li.innerText = message;
+  li.setAttribute('data-testid', 'message');
   messagesUl.appendChild(li);
 };
 
 const createUsuario = (message) => {
-  const messagesUl = document.querySelector('#usuario');
-  const li = document.createElement('li');
-  li.innerText = message;
-  messagesUl.appendChild(li);
+  const usuario = document.querySelector('#usuario');
+  usuario.setAttribute('data-testid', 'online-user');
+  usuario.innerText = message;
 };
 
 const createNewUsuario = (message) => {
   const messagesUl = document.querySelector('#usuarios');
   const li = document.createElement('li');
+  li.setAttribute('data-testid', 'online-user');
+  li.innerText = message;
+  messagesUl.appendChild(li);
+};
+
+const changeNick = (message) => {
+  const messagesUl = document.querySelector('#usuarios');
+  const li = document.createElement('li');
+  li.setAttribute('data-testid', 'online-user');
   li.innerText = message;
   messagesUl.appendChild(li);
 };
 
 socket.on('login', (mensagem) => createUsuario(mensagem));
 socket.on('newlogin', ({ usuario }) => createNewUsuario(usuario));
+socket.on('newNick', (usuario) => changeNick(usuario));
 socket.on('message', (message) => createMessage(message));
--- sockets/chat.js
+++ sockets/chat.js
@@ -12,11 +12,27 @@ const hora = () => {
   return `${date.getHours()}:${minutos}:${date.getSeconds()}`; 
 };
 
+ // Lara  - https://github.com/tryber/sd-010-b-project-webchat/pull/70/files
+const randomString = (length) => {
+    let nickname = '';
+    do {
+      nickname += Math.random().toString(36).substr(2);
+    } while (nickname.length < length);
+    nickname = nickname.substr(0, length);
+    return nickname;
+};
+  
 module.exports = (io) => io.on('connection', (socket) => {
-  socket.emit('login', `Seja bem vindo ao TryB-X Webchat, voce esta conectado como - ${socket.id}`);
-  socket.broadcast.emit('newlogin', { usuario: socket.id });
+  let newNickname = randomString(16);
+  socket.emit('login', newNickname);
+  socket.broadcast.emit('newlogin', { usuario: newNickname });
 
-  socket.on('message', ({ nickname = socket.id, chatMessage }) => {
+  socket.on('nick', (nick) => {
+    newNickname = nick;
+    io.emit('newNick', nick);
+  });
+
+  socket.on('message', ({ nickname = newNickname, chatMessage }) => {
     console.log(`${nickname} - Mensagem ${chatMessage}`, chatMessage);
     io.emit('message', `${data()} ${hora()} - ${nickname}: ${chatMessage}`);
   });
