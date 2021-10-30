75ddb06bba4775d904be2b08629eb40e8a2c2707
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-10-29 21:29:34 -0300

    refactor socket messages

--- src/sockets/messages.js
+++ src/sockets/messages.js
@@ -1,14 +1,13 @@
 const messagesModel = require('../../models/messagesModel');
 
-const dateNow = new Date().toLocaleString().replaceAll('/', '-');
-
 module.exports = (io) => io.on('connection', (socket) => {
   socket.on('message', ({ nickname, chatMessage }) => {
-    io.emit('message', `${dateNow} - ${nickname}: ${chatMessage}`);
+    const dateNow = new Date().toLocaleString().replaceAll('/', '-');
     messagesModel.CreateMessages({
       message: `${chatMessage}`,
       nickname: `${nickname}`,
       timestamp: dateNow,
     });
+    io.emit('message', `${dateNow} - ${nickname}: ${chatMessage}`);
   });
 });
