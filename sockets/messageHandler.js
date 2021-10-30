bbab620f3cf91e2cf271a0560ea3572c528cd7de
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-10-29 22:16:55 -0300

    integrando a função do evento message com o banco de dados

--- sockets/messageHandler.js
+++ sockets/messageHandler.js
@@ -1,10 +1,16 @@
 const { create } = require('../models/Messages');
 
 module.exports = (io, socket) => {
-  const formatMessage = (message) => {
-    const { chatMessage, nickname } = message;
-    const now = new Date().toLocaleString().replace(/\//g, '-');
-    io.emit('message', `${now} - ${nickname} ${chatMessage}`);
+  const formatTimestamp = (timestamp) => timestamp.toLocaleString().replace(/\//g, '-');
+  const createMessage = async (payload) => {
+    try {
+      const { chatMessage, nickname } = payload;
+      const { timestamp } = await create({ message: chatMessage, nickname });
+      const now = formatTimestamp(timestamp);
+      io.emit('message', `${now} - ${nickname} ${chatMessage}`);
+    } catch (error) {
+      console.error(error);
+    }
   };
-  socket.on('message', formatMessage);
+  socket.on('message', createMessage);
 };
