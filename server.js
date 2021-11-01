2ed2ccaffe28fd0885d0161bc52e6138a9f6822a
AdaoBJr <adaobjr@gmail.com>
2021-10-31 20:53:14 -0400

    feat: ✨ created server.js

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-// Faça seu código aqui
+const express = require('express');
+const http = require('http');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+
+const httpServer = http.createServer(app);
+
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000',
+    // endereco onde está rodando no front
+    method: ['GET', 'POST'],
+  },
+});
+
+const webChatIO = require('./socket/webChatSocket');
+
+webChatIO(io);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+// setando o express para utilizar o EJS
+
+app.set('views', path.join(__dirname, 'public/views'));
+
+const PORT = process.env.PORT || 3000;
+
+httpServer.listen(PORT, () => console.log(`Ouvindo a porta ${PORT}`));
