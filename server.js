bce00290be420a8cbb52c32bef80a5fe73168cec
tfeliperibeiro <thiagoribeiromixtv@gmail.com>
2021-11-01 18:31:37 -0300

    Socket conectado

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
-const path = require('path');
-const { chatController } = require('./controllers/chatController');
 
 const app = express();
+const path = require('path');
+
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST'],
+});
+
+io.on('connection', (socket) => {
+  console.log(socket.id);
+});
+
+const { chatController } = require('./controllers/chatController');
+
 require('dotenv').config();
 
 app.use(express.json());
@@ -12,4 +25,4 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(chatController);
 
-app.listen(process.env.PORT, () => console.log(`Servidor rodando na porta ${process.env.PORT}`));
+http.listen(process.env.PORT, () => console.log(`Servidor rodando na porta ${process.env.PORT}`));
