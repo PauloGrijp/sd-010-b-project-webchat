790176f30be8c9a1159837d937e77dc60d530d46
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-11-01 22:23:46 -0300

    feat: :sparkles: create app and http server

--- server.js
+++ server.js
@@ -1,1 +1,40 @@
-// Faça seu código aqui
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+// const path = require('path');
+
+const app = express();
+
+const { PORT = 3000 } = process.env;
+app.use(
+  cors({
+    origin: [`http://localhost:${PORT}`], // urls aceitas pelo cors
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // métodos aceitos pelas urls
+    allowedHeaders: ['Authorization'],
+  }),
+);
+
+const httpServer = require('http').createServer(app);
+
+const options = {
+  cors: {
+    origin: [`http://localhost:${PORT}`], // urls aceitas pelo cors
+    methods: ['GET', 'POST'], // métodos aceitos pelas urls
+  },
+};
+
+const io = require('socket.io')(httpServer, options);
+
+io.on('connection', (socket) => {
+  console.log(socket);
+  console.log(`Usuário conectado. ID: ${socket.id}`);
+});
+
+app.get('/', (req, res) => {
+  // res.sendFile(`${__dirname}/index.html`);
+  res.send('OI');
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
