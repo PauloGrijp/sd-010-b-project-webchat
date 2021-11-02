986b2b24697a7f4b32e27fd25b78a8d4573db382
Cristian Bugs <cristian.bugs@gmail.com>
2021-11-02 18:15:06 -0300

    fourth task: generate a list of online user and display them on the screen.

--- server.js
+++ server.js
@@ -29,28 +29,48 @@ app.set('views', './views');
         return result;
   }
   
-const io = require('socket.io')(http, {
+  const onlineUsers = [];
+  const newUser = () => {
+    const random = randomString(16);
+    onlineUsers.push(random);
+    return random;
+  };
+  const lastOnTop = (user) => {
+    const arrayUsers = onlineUsers.slice(0, -1);
+    arrayUsers.unshift(user);
+    return arrayUsers;
+  };
+
+  const io = require('socket.io')(http, {
     cors: {
       origin: 'http://localhost:3000', // url aceita pelo cors
       methods: ['GET', 'POST'], // Métodos aceitos pela url
     } });
-
-  io.on('connection', (socket) => {
-    console.log(`Usuário conectado. ID: ${socket.id} `);
-
-    socket.on('disconnect', () => console.log('Alguém se desconectou'));
-
-    socket.emit('random', randomString(16));
-
-    socket.on('message', ({ nickname, chatMessage }) => {
-    const dateNameMsg = `${todayDate()} ${nickname} ${chatMessage}`;
-    console
-    .log(`Alguém enviou a mensagem: ${dateNameMsg}`);
+    
+     // meus colegas Diegho, Renato e Carlos me ajudaram a função onlineUsers.indexOf(user);
+    io.on('connection', (socket) => {
+    let user = newUser();
+    lastOnTop(user);
+    
+    socket.on('disconnect', () => {
+      onlineUsers.splice(onlineUsers.indexOf(user), 1);
+      io.emit('onlineUser', onlineUsers);
+    });
+    
+    socket.emit('random', user);
+    
+    socket.emit('onlineUser', lastOnTop(user));
+    socket.broadcast.emit('onlineUser', onlineUsers);
+    
+    socket.on('newNickName', (newNickName) => { 
+      onlineUsers.splice(onlineUsers.indexOf(user), 1, newNickName);
+      user = newNickName;
+      io.emit('onlineUser', lastOnTop(user));
     });
 
     socket.on('message', ({ nickname, chatMessage }) => {
       const dateNameMsg = `${todayDate()} ${nickname} ${chatMessage}`;
-      io.emit('message', (dateNameMsg));
+      io.emit('message', (dateNameMsg)); 
     });
   });
 
