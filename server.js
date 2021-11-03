fd02fe3f444c07bda79bc45b52b2ef1626443713
Cristian Bugs <cristian.bugs@gmail.com>
2021-11-02 22:36:53 -0300

    added the || to the port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const app = express();
 const http = require('http').createServer(app);
