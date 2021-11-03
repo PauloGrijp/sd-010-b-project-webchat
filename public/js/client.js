1519d85367592d38954c4a7857ff8a92ec66e68c
Pablo Fernandes <pablofernandes@Owners-MacBook-Pro.local>
2021-11-03 01:07:40 -0300

    Starting requirement 2

--- public/js/client.js
+++ public/js/client.js
@@ -1,14 +1,38 @@
 const socket = window.io();
 
 const messageForm = document.querySelector('#messageForm');
+const nickForm = document.querySelector('#nickForm');
 const customAttr = 'data-testid';
-
 const messageBox = document.querySelector('#messageBox');
+
+const setUserName = (userName) => {
+    const oldUserName = localStorage.getItem('userName');
+    const newUserName = !userName ? Math.random().toString(36) : userName;
+    localStorage.setItem('userName', newUserName);
+    if (userName) {
+        socket.emit('setup', { oldUserName, newUserName });
+    }
+    const nickNameLabel = document.querySelector('#nickname');
+    nickNameLabel.innerText = `Current Nickname: ${newUserName}`;
+    return newUserName;
+};
+
+setUserName();
 messageBox.focus();
+
+nickForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const nicknameBox = document.querySelector('#change-nick-text');
+    setUserName(nicknameBox.value);
+    nicknameBox.value = '';
+    return false;
+});
 
 messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    socket.emit('userMessage', messageBox.value);
+    const nickname = localStorage.getItem('userName');
+    const chatMessage = messageBox.value;
+    socket.emit('userMessage', { chatMessage, nickname });
     messageBox.value = '';
     return false;
 });
--- sockets/chatServer.js
+++ sockets/chatServer.js
@@ -1,18 +1,24 @@
-module.exports = (io) => {
-    const getDateTime = () => {
-        const now = new Date();
-        const AMPM = now.getHours() >= 12 ? 'PM' : 'AM'; 
-        let fullDateTime = `${now.getDate()}-${now.getMonth() + 1}-${now.getFullYear()} `;
-        fullDateTime += `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} ${AMPM}`;
-        return fullDateTime;
-    };
+const getDateTime = () => {
+    const now = new Date();
+    const AMPM = now.getHours() >= 12 ? 'PM' : 'AM'; 
+    let fullDateTime = `${now.getDate()}-${now.getMonth() + 1}-${now.getFullYear()} `;
+    fullDateTime += `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} ${AMPM}`;
+    return fullDateTime;
+};
 
+module.exports = (io) => {
     io.on('connection', (socket) => {
-        socket.broadcast.emit(`User ${socket.id} had just connected`);
+        socket.broadcast.emit('message', 'Server: a new user has just connected');
         socket.on('userMessage', (message) => {
             const dateTime = getDateTime();
-            const messageRecord = `${dateTime} - ${socket.id}: ${message}`;
-            io.emit('message', messageRecord);
+            const formattedMessage = `${dateTime} - ${message.nickname}: ${message.chatMessage}`;
+            io.emit('message', formattedMessage);
+        });
+        socket.on('setup', ({ oldUserName, newUserName }) => {
+            socket.broadcast.emit(
+                'message',
+                `User ${oldUserName} has changed his/her nickname. Now it's ${newUserName}`,
+            );
         });
     });
 };
