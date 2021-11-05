b1887211f7b843e84668138c90dfbf3312b44df0
Rafael <rmathscoach@gmail.com>
2021-11-05 17:55:07 -0300

    configuro server

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-// Faça seu código aqui
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+
+const PORT = process.env.PORT || 3000;
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST']
+  }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(PORT, () => {
+  console.log(`Rodando fino na porta ${PORT}`);
+});
