f1cf779e6c1b87de6fc924be2b03e4108e71eaaf
Rafael <rmathscoach@gmail.com>
2021-11-05 20:39:40 -0300

    requisito 2 em desenvolvimento

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const moment = require('moment');
 
 const PORT = process.env.PORT || 3000;
 
+const usersOnline = {};
+
 const io = require('socket.io')(http, {
   cors: {
     origin: `http://localhost:${PORT}`,
@@ -14,11 +16,24 @@ const io = require('socket.io')(http, {
 
 // função executada quando usuário se conecta
 io.on('connection', (socket) => {
+  usersOnline[socket.id] = socket.id.slice(0, 16);
   socket.on('message', ({ chatMessage, nickname }) => {
     const dateNow = moment().format('DD-MM-YYYY HH:mm:ss');
     io.emit('message', `${dateNow} - ${nickname}: ${chatMessage}`);
   });
+
+  socket.on('disconnect', () => {
+    delete usersOnline[socket.id];
+    io.emit('users', Object.values(usersOnline));
+  });
+
+  socket.on('newNickName', (nickname) => {
+    usersOnline[socket.id] = nickname;
+    io.emit('users', Object.values(usersOnline));
+  });
 });
+
+io.emit('usersOn', Object.values(usersOnline));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
---
+++ views/scripts.js
@@ -0,0 +1,6 @@
+const socket = window.io();
+
+const messageForm = document.querySelector('#message_form');
+const nickNameForm = document.querySelector('#nickname_form');
+const messageInput = document.querySelector('#message_input');
+const nickNameInput = document.querySelector('#nickname_input');
