094ddc3b6ef855289aaeb633816be9fc5ada23a4
Emerson Saturnino <programador.emerson@hotmail.com>
2021-11-05 22:23:54 -0300

    VARIOS USUARIOS ONLINE

--- sockets/index.js
+++ sockets/index.js
@@ -3,6 +3,7 @@ const users = [];
 const webChat = (io) => {
   io.on('connection', (socket) => {
     socket.on('login', (nickname) => {
+      console.log('New user connected');
       users.push({ nickname, socketId: socket.id });
       io.emit('users', users);
     });
