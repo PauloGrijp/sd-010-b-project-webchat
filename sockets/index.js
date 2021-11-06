163c45c08d08bbf3e1f6a328b19695ad2e66b74c
Emerson Saturnino <programador.emerson@hotmail.com>
2021-11-05 22:40:04 -0300

    Teste de requisitos

--- sockets/index.js
+++ sockets/index.js
@@ -14,7 +14,6 @@ const updateName = (socket, io) => {
 const webChat = (io) => {
   io.on('connection', (socket) => {
     socket.on('login', (nickname) => {
-      console.log('New user connected');
       users.push({ nickname, socketId: socket.id });
       io.emit('users', users);
     });
