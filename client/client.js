bcfafa378e415e95cff5e55c3005eed54709614c
Roger Franco <rogerfrancogalt@gmail.com>
2021-11-07 16:08:49 -0400

    req4

--- client/client.js
+++ client/client.js
@@ -17,7 +17,7 @@ const getChatty = () => chatty;
 const excludeChatty = (socket) => {
   const index = chatty.findIndex((item) => item.id === socket.id);
   chatty.splice(index, 1);
-  console.log(chatty);
+  return chatty;
 };
 
 module.exports = { addChatty, getChatty, editChatty, excludeChatty };
