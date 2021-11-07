d93170d3dfe218d823fe12481ca0d4e5074fa39f
Caio <caioqs.info@gmail.com>
2021-11-06 21:36:36 -0300

    Feito requisito 1 e 2

--- server.js
+++ server.js
@@ -1,1 +1,38 @@
-// Iniciando
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+const cors = require('cors');
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+const arrayUser = [];
+
+io.on('connection', (socket) => {
+  socket.on('message', ({ chatMessage, nickname }) => {
+    const date = (new Date()).toLocaleString().replace(/\//g, '-');
+    io.emit('message', `${date} - ${nickname}: ${chatMessage}`);
+ });
+
+  socket.on('usersOnline', ({ nickname }) => {
+    const obj = { nickname, id: socket.id };
+    arrayUser.push(obj);
+    console.log(arrayUser);
+    io.emit('online', arrayUser);
+  });
+});
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+http.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+}); 
