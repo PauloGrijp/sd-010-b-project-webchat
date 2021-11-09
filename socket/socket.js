47613ad621bf7ad3f665776f3050b5209d161b5c
Jonathan Souza <souzajonathan570@gmail.com>
2021-11-09 20:51:47 -0300

    feat: added option to update nickname

--- socket/socket.js
+++ socket/socket.js
@@ -1,25 +1,46 @@
 const moment = require('moment');
 const Model = require('../models/chatModel');
 
-const client = {};
+const users = [];
+
+const setUser = (user) => {
+  users.push(user);
+
+  return user;
+};
+
+const updateUser = (obj) => {
+  const index = users.indexOf(obj.oldNickname);
+  users.splice(index, 1, obj.newNickname);
+};
+
+const message = (chatMessage, nickname) => ({
+    message: `${moment().format('DD-MM-YYYY HH:mm:ss')} ${nickname}: ${chatMessage}`,
+    nickname,
+    timestamp: moment().format('YYYY-MM-DD HH:mm:ss'),
+  });
+
+const NEW_CONNETION = 'new-connection';
 
 module.exports = (io) => io.on('connection', async (socket) => {
-  client[socket.id] = socket.id.slice(1, 17);
+  socket.emit('user-nickname', setUser(socket.id.slice(1, 17)));
+  
+  io.emit(NEW_CONNETION, users);
 
-  socket.emit('new-user', client[socket.id]);
-
-  socket.on('message', ({ chatMessage, nickname }) => {
-    const msg = {
-      message: `${moment().format('DD-MM-YYYY HH:mm:ss')} ${nickname}: ${chatMessage}`,
-      nickname,
-      timestamp: moment().format('YYYY-MM-DD HH:mm:ss'),
-    };
+  socket.on('message', async ({ chatMessage, nickname }) => {
+    const msg = message(chatMessage, nickname);
 
     io.emit('message', msg.message);
-    Model.sendMessage(msg);
+    await Model.sendMessage(msg);
   });
 
   socket.on('update-nickname', (nick) => {
-    client[socket.id] = nick;
+    updateUser(nick);
+    io.emit(NEW_CONNETION, users);
+  });
+
+  socket.on('disconnect', (user) => {
+    users.splice(users.indexOf(user), 1);
+    io.emit(NEW_CONNETION, users);
   });
 });
